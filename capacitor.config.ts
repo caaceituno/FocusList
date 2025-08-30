07a4c1ff516da1f54785592c146e68b2cf4cd646
Camila <ca.aceituno@duocuc.cl>
2025-08-30 18:15:09 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FocusList',
+  webDir: 'www'
+};
+
+export default config;
