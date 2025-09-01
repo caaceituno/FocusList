ca692ece5759e7ea1b2d100aef7d08cd64e80ec4
okedux <be.ojedao@duocuc.cl>
2025-08-31 20:21:03 -0400

    implementacion firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBPpitVorhw89quOLrkj9WWdc5FVCJqK4I",
+    authDomain: "trekin-1ad46.firebaseapp.com",
+    projectId: "trekin-1ad46",
+    storageBucket: "trekin-1ad46.appspot.com",
+    messagingSenderId: "812550878961",
+    appId: "1:812550878961:android:66bd3569d523d7121d1b50"
+  }
 };
 
 /*
