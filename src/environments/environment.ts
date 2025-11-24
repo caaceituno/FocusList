65a0fe7291acbcef86b4db835e2c78048015b006
Camila <ca.aceituno@duocuc.cl>
2025-11-24 02:56:47 -0300

    Eliminar carpeta environments del repositorio

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  emailServiceID: 'service_c0v2bzh',
-  emailTemplateID: 'template_66chkof',
-  emailPublicKey: '_MVTE2n0GrMN9bl1o',
-  climaApiKey: 'b08f7a22e98b4ff8b9d54017252411'
-};
